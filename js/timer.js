6c3bfa0987f56bb7b03e2648f8ee3271f12ca425
Simon Rehwald <simon.rehwald@tum.de>
2017-05-19 23:51:14 +0200

    seconds can be removed from timer

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,6 @@
+var displaySeconds = true;
+
+
 function start() {
     // TODO
 }
@@ -6,21 +9,58 @@ function stop() {
     // TODO
 }
 
+function handleSeconds(checkbox) {
+    if (checkbox.checked == true) {
+        displaySeconds = false;
+    } else {
+        displaySeconds = true;
+    }
+    
+    m.redraw();
+}
+
 var Timer = {
-    view: function() {
-        return m("div", [
-            m("div", {style: "display: inline-block"}, [
-                m("h6", {class: "subtitle is-6"}, "MIN"),
-                m("h1", {class: "title is-1", style: "font-size: 8em"}, "1337")
-            ]),
-            m("div", {style: "margin-left: 1.5em; display: inline-block"}, [
-                m("h1", {class: "title is-1", style: "font-size: 8em"}, ":")
-            ]),
-            m("div", {style: "margin-left: 1.5em; display: inline-block"}, [
-                m("h6", {class: "subtitle is-6"}, "SEC"),
-                m("h1", {class: "title is-1", style: "font-size: 8em"}, "42")
+    view: function (vnode) {
+        var minutes = m("div", {
+            style: "display: inline-block"
+        }, [
+                m("h6", {
+                class: "subtitle is-6"
+            }, "MIN"),
+                m("h1", {
+                class: "title is-1",
+                style: "font-size: 8em"
+            }, "1337")
+            ]);
+        var seconds = m("div", {
+            style: "margin-left: 1.5em; display: inline-block"
+        }, [
+                m("h6", {
+                class: "subtitle is-6"
+            }, "SEC"),
+                m("h1", {
+                class: "title is-1",
+                style: "font-size: 8em"
+            }, "42")
+            ]);
+
+        if (displaySeconds)
+            return m("div", [
+                minutes,
+                m("div", {
+                    style: "margin-left: 1.5em; display: inline-block"
+                }, [
+                    m("h1", {
+                        class: "title is-1",
+                        style: "font-size: 8em"
+                    }, ":")
+                ]),
+                seconds
             ])
-        ])
+        else
+            return m("div", [
+                minutes
+            ])
     }
 }
 
